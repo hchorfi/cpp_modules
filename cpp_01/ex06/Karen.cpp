584aea0c68a5ec3bf404a0ffef244b417b772f4d
Hamza Chorfi <hchorfi@e0r3p11.1337.ma>
2021-07-02 21:55:00 +0100

    prepush

--- cpp_01/ex06/Karen.cpp
+++ cpp_01/ex06/Karen.cpp
@@ -12,22 +12,22 @@ Karen::~Karen(void)
 
 void    Karen::_debug(void)
 {
-    std::cout << Blue <<"I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!\n";
+    std::cout << Blue <<"I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!\n" << Reset;
 }
 
 void    Karen::_info(void)
 {
-    std::cout << Green << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!\n";
+    std::cout << Green << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!\n" << Reset;
 }
 
 void    Karen::_warning(void)
 {
-    std::cout << Yellow << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month.\n";
+    std::cout << Yellow << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month.\n" << Reset;
 }
 
 void    Karen::_error(void)
 {
-    std::cout << Red << "This is unacceptable, I want to speak to the manager now.\n";
+    std::cout << Red << "This is unacceptable, I want to speak to the manager now.\n" << Reset;
 }
 
 void Karen::complain( std::string level )
--- cpp_01/ex06/main.cpp
+++ cpp_01/ex06/main.cpp
@@ -1,11 +1,43 @@
 #include "Karen.hpp"
 
-int main()
+#define INFO 0
+#define WARNING 1
+#define ERROR 2
+#define DEBUG 3
+
+int main(int argc, char **argv)
 {
+    if (argc < 2)
+    {
+        std::cout << "Please Enter One Argument\n" << Reset ;
+        return 1;
+    }
+    
     Karen ka;
-    ka.complain("INFO");
-    ka.complain("WARNING");
-    ka.complain("ERROR");
-    ka.complain("DEBUG");
+    std::string level = argv[1];
+    std::string levelList[4] = {"INFO", "WARNING", "ERROR", "DEBUG"};
+    int i = 0;
+    while (i < 4)
+    {
+        if (level == levelList[i])
+            break;
+        i++;
+    }
+    
+    switch (i)
+    {
+    case ERROR :
+        ka.complain("ERROR");
+    case WARNING :
+        ka.complain("WARNING");
+    case INFO :
+        ka.complain("INFO");
+    case DEBUG :
+        ka.complain("DEBUG");
+        break;
+    default:
+        std::cout << "Probably complaining about insignificant problems\n";
+        break;
+    }
     return 0;
 }
